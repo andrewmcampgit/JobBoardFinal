838a5358ca102403ed27490d00fe4ee4499cfc3e
Andrew Camp <Andrew.Camp@vu.com>
2020-06-04 16:27:44 -0500

    fixed footer and finished applications views

--- JobBoard.UI.MVC/Controllers/LocationsController.cs
+++ JobBoard.UI.MVC/Controllers/LocationsController.cs
@@ -10,12 +10,12 @@ using JobBoard.Data.EF;
 
 namespace JobBoard.UI.MVC.Controllers
 {
-    [Authorize(Roles = "Admin")]
+    
     public class LocationsController : Controller
     {
         private JobBoardEntities db = new JobBoardEntities();
 
-        [Authorize(Roles = "Admin, Manager")]
+        
         // GET: Locations
         public ActionResult Index()
         {
