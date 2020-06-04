1e8859562561b24b3bea2b1a1c45dc68b9740c44
Andrew Camp <Andrew.Camp@vu.com>
2020-06-03 19:50:42 -0500

    Worked on Shared Layout and view lockdowns

--- JobBoard.UI.MVC/App_Start/BundleConfig.cs
+++ JobBoard.UI.MVC/App_Start/BundleConfig.cs
@@ -19,11 +19,32 @@ namespace JobBoard.UI.MVC
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                "~/Content/assets/vendor/jquery/jquery.min.js",
+                "~/Content/assets/vendor/bootstrap/js/bootstrap.bundle.min.js",
+                "~/Content/assets/vendor/jquery.easing/jquery.easing.min.js",
+                "~/Content/assets/vendor/php-email-form/validate.js",
+                "~/Content/assets/vendor/jquery-sticky/jquery.sticky.js",
+                "~/Content/assets/vendor/isotope-layout/isotope.pkgd.min.js",
+                "~/Content/assets/vendor/venobox/venobox.min.js",
+                "~/Content/assets/vendor/waypoints/jquery.waypoints.min.js",
+                "~/Content/assets/vendor/owl.carousel/owl.carousel.min.js",
+                "~/Content/assets/vendor/aos/aos.js",
+                "~/Content/assets/js/main.js",
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                
+                "~/Content/assets/vendor/bootstrap/css/bootstrap.min.css",
+                "~/Content/assets/vendor/icofont/icofont.min.css",
+                "~/Content/assets/vendor/boxicons/css/boxicons.min.css",
+                "~/Content/assets/vendor/animate.css/animate.min.css",
+                "~/Content/assets/vendor/venobox/venobox.css",
+                "~/Content/assets/vendor/owl.carousel/~/Content/assets/owl.carousel.min.css",
+                "~/Content/assets/vendor/aos/aos.css",
+                "~/Content/assets/vendor/remixicon/remixicon.css",
+                "~/Content/assets/css/style.css",
+                "~/Content/bootstrap.css",
                       "~/Content/site.css"));
         }
     }
